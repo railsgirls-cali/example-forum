18c694342e292fe2b3fe92d3587d90b94a83d192
JesusEduardo2028 <jesusduardo2028@gmail.com>
2016-04-28 11:08:18 -0500

    sort topics on index view

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,7 +4,7 @@ class TopicsController < ApplicationController
   # GET /topics
   # GET /topics.json
   def index
-    @topics = Topic.all
+    @topics = Topic.all.order_by(created_at: :desc)
     @topic = Topic.new
   end
 
